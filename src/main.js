59d812f6f82b7c52caf64562a5d0194a0e3af178
leandro <leandro.korb69@gmail.com>
2024-10-17 23:15:53 -0300

    first routes

--- src/main.js
+++ src/main.js
@@ -1,6 +1,23 @@
-import './assets/main.css'
+import './assets/main.css';
+import { createApp } from 'vue';
+import App from './App.vue';
 
-import { createApp } from 'vue'
-import App from './App.vue'
+const routes = createRouter[
+    {path: "/", component: login},
+    {path: "/home", component: home},
+    {path: "/profile/:id", component: Profile}
+];
+
+const router = createRouter({
+    routes,
+    history: createWebHistory(),
+})
+
+router.beforeEach((to,from) =>{
+    console.log(to);
+    return true;
+})
+
+createApp(App).use(router).mount('#app')
 
 createApp(App).mount('#app')
