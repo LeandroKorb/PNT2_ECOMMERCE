5b001d26731fb60114d565ef4ff3c0160468d32a
leandro <leandro.korb69@gmail.com>
2024-10-22 00:43:50 -0300

    se agrego filtro y busqueda de productos

--- src/components/Productos.vue
+++ src/components/Productos.vue
@@ -1,5 +1,28 @@
 <script setup>
-import {list_products} from '../models/listproducts.js';
+import { productsList } from  '../models/productsList.js'
+import { computed } from 'vue';
 
-const listProduct = list_products;
+const{searchQuery} = defineProps({
+  searchQuery: String
+})
+
+const filteredProducts = computed(() => {
+  return productsList.filter(product =>
+    product.nombre.toLowerCase().includes(searchQuery.toLowerCase())
+  )
+})
+
 </script>
+
+<template>
+<h1>{{ searchQuery }}</h1>
+
+<ul class="product-list">
+      <li v-for="product in filteredProducts" :key="product.id" class="product-item">
+        <img :src="product.img" :alt="product.nombre" />
+        <p>{{ product.nombre }}</p>
+        <p> {{ product.id }}</p>
+        <p>{{ product.precio }}</p>
+      </li>
+    </ul>
+</template>
